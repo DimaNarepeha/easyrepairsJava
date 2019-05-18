26556793fbe8a2f2980589bb6230422dac62efe0
Anastasia Hanuliak <anastasia.hanuliak@gmail.com>
2019-05-18 20:55:18 +0300

    modified provider filter method

--- src/main/java/com/softserve/demo/filter/ProviderFilter.java
+++ src/main/java/com/softserve/demo/filter/ProviderFilter.java
@@ -2,8 +2,10 @@ package com.softserve.demo.filter;
 
 import com.softserve.demo.filter.specification.ProviderSpecification;
 import com.softserve.demo.model.Provider;
+import com.softserve.demo.model.ProviderStatus;
 import com.softserve.demo.model.search.ProviderCriteria;
 import com.softserve.demo.service.impl.ProvidersServiceImpl;
+import com.softserve.demo.util.ProviderInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,8 @@ import java.util.stream.Collectors;
 @Service
 public class ProviderFilter {
 
+
+
     private final ProvidersServiceImpl providersService;
 
     public ProviderFilter(ProvidersServiceImpl providersService) {
@@ -21,17 +25,21 @@ public class ProviderFilter {
     }
 
     public List<Provider> findByListServices(ProviderCriteria criteria) {
-        return findByAllCriteria(criteria)
+        return findByCriteria(criteria.getStatus(), criteria.getCity(), criteria.getMinRaiting(), criteria.getMaxRaiting())
                 .stream()
                 .filter(p -> p.getServices().containsAll(criteria.getServices()))
                 .collect(Collectors.toList());
     }
 
-    public List<Provider> findByAllCriteria(ProviderCriteria criteria) {
-        return providersService.findAll(Specification.where(ProviderSpecification.isStatus(criteria.getStatus()))
-                .and(ProviderSpecification.isCity(criteria.getCity()))
-                .and(ProviderSpecification.betweenRangeRaiting(criteria.getMinRaiting(), criteria.getMaxRaiting())));
+    public List<Provider> findByCriteria(ProviderStatus status, String city,  int minRaiting, int maxRaiting) {
+        return providersService.findAll(Specification.where(ProviderSpecification.isStatus(status))
+                .and(ProviderSpecification.isCity(city))
+                .and(ProviderSpecification.betweenRangeRaiting(minRaiting, maxRaiting)));
 
+    }
+
+    public List<Provider> findAllApproved(){
+        return providersService.findAllByStatus(ProviderStatus.APPROVED);
     }
 
 }
