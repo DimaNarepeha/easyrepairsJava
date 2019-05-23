f9e1948f6c9dff7abfa868bcdc78a2599a302985
Anastasia Hanuliak <anastasia.hanuliak@gmail.com>
2019-05-23 04:21:20 +0300

    add landing page controller

--- src/main/java/com/softserve/demo/controller/LandingPageController.java
+++ src/main/java/com/softserve/demo/controller/LandingPageController.java
@@ -1,19 +1,20 @@
 package com.softserve.demo.controller;
 
 
+import com.softserve.demo.dto.FeedbackGeneralDTO;
+import com.softserve.demo.dto.ProviderCriteriaDTO;
 import com.softserve.demo.dto.ServiceProviderInfoDTO;
 import com.softserve.demo.filter.ProviderFilter;
-import com.softserve.demo.model.ProviderStatus;
-import com.softserve.demo.model.Service;
-import com.softserve.demo.model.search.ProviderCriteria;
-import com.softserve.demo.repository.ServicesRepository;
+import com.softserve.demo.service.impl.FeedbackServiceImpl;
+import com.softserve.demo.util.FeedbackGeneralMapper;
+import com.softserve.demo.util.ProviderCriteriaMapper;
 import com.softserve.demo.util.ProviderInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -21,41 +22,37 @@ import java.util.List;
 @RequestMapping("landing-page")
 @ResponseBody
 public class LandingPageController {
-   private final ProviderFilter filter;
-   private  final ProviderInfoMapper mapper;
+    private final ProviderFilter filter;
+    private final ProviderInfoMapper providerInfoMapper;
+    private final ProviderCriteriaMapper providerCriteriaMapper;
+    private final FeedbackServiceImpl feedbackService;
+
 
     @Autowired
-    ServicesRepository sericee;
-
-    @Autowired
-    public LandingPageController(ProviderFilter filter, ProviderInfoMapper mapper) {
+    public LandingPageController(ProviderFilter filter, ProviderInfoMapper providerInfoMapper, FeedbackGeneralMapper feedbackMapper, ProviderCriteriaMapper providerCriteriaMapper, FeedbackServiceImpl feedbackService) {
         this.filter = filter;
-        this.mapper = mapper;
-    }
-
-
-    @GetMapping("test")
-    public ResponseEntity<List<ServiceProviderInfoDTO>> test() {
-
-        List<Service> ss = new ArrayList<>();
-        ss.add(sericee.getOne(1));
-        ss.add(sericee.getOne(3));
-
-        ProviderCriteria criteria = new ProviderCriteria();
-        criteria.setCity("Lviv");
-        criteria.setMaxRaiting(4);
-        criteria.setMinRaiting(1);
-        criteria.setStatus(ProviderStatus.APPROVED);
-        criteria.setServices(ss);
-
-        return new ResponseEntity<>(mapper.map(filter.findByListServices(criteria)), HttpStatus.OK);
+        this.providerInfoMapper = providerInfoMapper;
+        this.providerCriteriaMapper = providerCriteriaMapper;
+        this.feedbackService = feedbackService;
     }
 
 
     @GetMapping("all-approved")
-    public ResponseEntity<List<ServiceProviderInfoDTO>> getAllApproved(){
-        return new ResponseEntity<>(mapper.map(filter.findAllApproved()), HttpStatus.OK);
+    public ResponseEntity<List<ServiceProviderInfoDTO>> getAllApproved() {
+        return new ResponseEntity<>(providerInfoMapper.map(filter.findAllApproved()), HttpStatus.OK);
     }
+
+    @GetMapping("top-comment")
+    public ResponseEntity<List<FeedbackGeneralDTO>> getTopComment() {
+        return new ResponseEntity<>(feedbackService.findTop4ByCreatedDateBefore(Calendar.getInstance()), HttpStatus.OK);
+    }
+
+    @PostMapping("filter")
+    public ResponseEntity<?> filter(@RequestBody ProviderCriteriaDTO criteria) {
+        System.out.println(filter.findByListServices(providerCriteriaMapper.ProviderCriteriaDTOToProviderCriteria(criteria)).toString());
+        return new ResponseEntity<>(filter.findByListServices(providerCriteriaMapper.ProviderCriteriaDTOToProviderCriteria(criteria)), HttpStatus.OK);
+    }
+
 
 }
 
