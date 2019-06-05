7803af97d2fe7fa12b13ee9a0bd2913debf32f27
Anastasiia Hanuliak <anastasia.hanuliak@gmail.com>
2019-06-05 14:40:12 +0300

    get last 4 comment

---
+++ src/main/java/com/softserve/demo/service/impl/FeedbackServiceImpl.java
@@ -0,0 +1,31 @@
+package com.softserve.demo.service.impl;
+
+import com.softserve.demo.dto.FeedbackGeneralDTO;
+import com.softserve.demo.repository.FeedbackRepository;
+import com.softserve.demo.service.FeedbackService;
+import com.softserve.demo.util.FeedbackGeneralMapper;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.List;
+
+@Service
+public class FeedbackServiceImpl implements FeedbackService {
+
+
+    final private FeedbackGeneralMapper feedbackMapper;
+    final private FeedbackRepository feedbackRepository;
+
+    public FeedbackServiceImpl(FeedbackGeneralMapper feedbackMapper, FeedbackRepository feedbackRepository) {
+        this.feedbackMapper = feedbackMapper;
+        this.feedbackRepository = feedbackRepository;
+    }
+
+
+    @Override
+    public List<FeedbackGeneralDTO> findTop4ByCreatedDateBefore(LocalDateTime createdDate) {
+        return feedbackMapper.map(feedbackRepository.findTop4ByCreatedDateBefore(createdDate));
+    }
+}
