e62f4d2b09ac7cf144f4c8c5ef1cec3820429c51
junior <pitbullboks@gmail.com>
2019-06-06 13:39:20 +0300

    JAVALV-260:Restore password functionality

---
+++ src/main/java/com/softserve/demo/controller/PasswordRecoveryController.java
@@ -0,0 +1,22 @@
+package com.softserve.demo.controller;
+
+import com.softserve.demo.service.RecoveryPasswordService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PasswordRecoveryController {
+
+    private final RecoveryPasswordService recoveryPasswordService;
+
+    public PasswordRecoveryController(RecoveryPasswordService recoveryPasswordService) {
+        this.recoveryPasswordService = recoveryPasswordService;
+    }
+
+    @PostMapping("/recovery")
+    public String sendEmailTo(@RequestParam final String email) {
+        recoveryPasswordService.passwordRecovery(email);
+        return "Successfully sent!";
+    }
+}
