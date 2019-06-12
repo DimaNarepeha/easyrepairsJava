763e7ff47f90b788f432d43c5b562bb1ccca5d40
AndriY <andriyxxx357@gmail.com>
2019-06-12 18:50:46 +0300

    fix customer add validation for offer and order

--- src/main/java/com/softserve/demo/dto/OfferDTO.java
+++ src/main/java/com/softserve/demo/dto/OfferDTO.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,12 +14,15 @@ import java.util.List;
 @Getter
 @Setter
 public class OfferDTO {
+
     private Integer id;
     private String description;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createDate;
+    @FutureOrPresent
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @FutureOrPresent
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     @NotNull
@@ -25,5 +30,6 @@ public class OfferDTO {
     @NotNull
     private LocationDTO locationDTO;
     @NotNull
+    @Size(min = 1)
     private List<ServiceDTO> serviceDTOs;
 }
--- src/main/java/com/softserve/demo/dto/OrderDTO.java
+++ src/main/java/com/softserve/demo/dto/OrderDTO.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,18 +16,28 @@ import java.util.List;
 @Setter
 @ToString
 public class OrderDTO {
+
     private Integer id;
     @NotNull
+    @Size(min = 2, max = 1000)
     private String description;
+    @Size(max = 1000)
     private String extraDetails;
+    @Size(max = 1000)
     private String price;
+    @NotNull
+    @Size(max = 30)
     private String timeRequirement;
+    @Size(max = 30)
     private String customerApproved;
+    @Size(max = 30)
     private String providerApproved;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createDate;
+    @FutureOrPresent
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @FutureOrPresent
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     @NotNull
@@ -35,5 +47,6 @@ public class OrderDTO {
     @NotNull
     private LocationDTO locationDTO;
     @NotNull
+    @Size(min = 1)
     private List<ServiceDTO> serviceDTOs;
 }
