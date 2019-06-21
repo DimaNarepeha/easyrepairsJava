72fdae5982291db5e2e9fcd45ce00a4f77f32a4b
serhiylev <slevitsku@gmail.com>
2019-06-21 13:11:47 +0300

    fix issues from pull request

--- src/main/java/com/softserve/demo/model/Customer.java
+++ src/main/java/com/softserve/demo/model/Customer.java
@@ -1,6 +1,8 @@
 package com.softserve.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
