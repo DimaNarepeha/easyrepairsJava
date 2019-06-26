1499b1d24d4bae8be5e79c2269114636fa43b2d9
Oleksandr <ssilich@bitbucket.org>
2019-06-26 14:21:40 +0300

    fix problem with CORS

--- src/main/java/com/softserve/demo/configuration/security/AuthorizationServerConfig.java
+++ src/main/java/com/softserve/demo/configuration/security/AuthorizationServerConfig.java
@@ -1,5 +1,6 @@
 package com.softserve.demo.configuration.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,10 +10,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private static final String SIGNING_KEY = "as466kf";
 
     private final UserDetailsService userDetailsService;
     private final JdbcTemplate jdbcTemplate;
@@ -28,14 +32,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         this.tokenStore = tokenStore;
     }
 
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY);
+        return converter;
+    }
+
     @Override
     public void configure(final ClientDetailsServiceConfigurer configurer) throws Exception {
         configurer.jdbc(jdbcTemplate.getDataSource());
     }
-
     @Override
     public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints.tokenStore(tokenStore)
+                .accessTokenConverter(accessTokenConverter())
                 .authenticationManager(customAuthenticationProvider)
                 .userDetailsService(userDetailsService);
     }
